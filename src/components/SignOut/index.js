9508c2cbd639bce665b625482927900fb887d5e8
João Ceia <joao.p.ceia@gmail.com>
2021-05-02 20:22:37 +0100

    fix in import

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,4 +1,4 @@
-import { withFirebase } from 's../Firebase';
+import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
   <buton type="button" onClick={firebase.doSignOut}>
