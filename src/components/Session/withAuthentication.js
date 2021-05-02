d746dc9f89a7da8da170507efa7301e23ea5bf40
João Ceia <joao.p.ceia@gmail.com>
2021-05-02 21:01:12 +0100

    added withAuthentication HOC

---
+++ src/components/Session/withAuthentication.js
@@ -0,0 +1,10 @@
+const withAuthentication = Component => {
+  class WithAuthentication extends React.Component {
+    render() {
+      return <Component {...this.props} />;
+    }
+  }
+  return WithAuthentication;
+};
+
+export default withAuthentication;
