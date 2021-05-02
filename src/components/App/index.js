b1fad581f93f5749cc8a731313b87951f0a5f251
João Ceia <joao.p.ceia@gmail.com>
2021-05-02 20:57:55 +0100

    added AuthUser context to App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,6 +10,7 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
+import { AuthUserContext } from '../Session';
 
 const App = (props) => {
   const [authUser, setAuthUser] = useState(null);
@@ -23,19 +24,21 @@ const App = (props) => {
   }, []);
 
   return (
-    <Router>
-      <Navigation authUser={authUser} />
-      <hr />
-      <Switch>
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.PASSWORD_FORGET} components={PasswordForgetPage} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
-      </Switch>
-    </Router>
+    <AuthUserContext.Provider value={authUser}>
+      <Router>
+        <Navigation />
+        <hr />
+        <Switch>
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} components={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </Switch>
+      </Router>
+    </AuthUserContext.Provider>
   );
 };
 
