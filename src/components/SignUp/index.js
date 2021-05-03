21d51b247575f6e50994183f0634f0a982951457
João Ceia <joao.p.ceia@gmail.com>
2021-05-03 05:59:39 +0100

    changed SignUpForm to use hooks

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 import { withFirebase } from '../Firebase';
@@ -22,91 +22,80 @@ const INITIAL_STATE = {
   error: null,
 };
 
+const SignUpFormBase = (props) => {
+  const [values, setValues] = useState({ ...INITIAL_STATE });
 
-// TODO: replace by functional version
-class SignUpFormBase extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
-  
-  onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault();
-
-    const { email, passwordOne } = this.state;
+    const { email, passwordOne } = values;
     
-    this.props.firebase
+    props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
-        this.setState({ ...INITIAL_STATE })
+        setValues({ ...INITIAL_STATE });
 
         // TODO: replace with useHistory hook
-        this.props.history.push(ROUTES.HOME);
+        props.history.push(ROUTES.HOME);
       })
-      .catch(error => this.setState({ error }));
+      .catch(error => setValues({ ...values, error }));
   }
   
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  const onChange = event => {
+    setValues({ ...values, [event.target.name]: event.target.value });
   };
-  
-  render() {
-    
-    const {
-      username,
-      email,
-      passwordOne,
-      passwordTwo,
-      error,
-    } = this.state;
 
-    // The user is only allowed to sign up if both passwords are the same, and
-    // if the username, email and at least one password are filled with a string
-    const isInvalid =
-      passwordOne !== passwordTwo ||
-      passwordOne === '' ||
-      email === '' ||
-      username === '';
+  const {
+    username,
+    email,
+    passwordOne,
+    passwordTwo,
+    error,
+  } = values;
 
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign Up
-        </button>
+  // The user is only allowed to sign up if both passwords are the same, and
+  // if the username, email and at least one password are filled with a string
+  const isInvalid =
+    passwordOne !== passwordTwo ||
+    passwordOne === '' ||
+    email === '' || username === '';
 
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="username"
+        value={username}
+        onChange={onChange}
+        type="text"
+        placeholder="Full Name"
+      />
+      <input
+        name="email"
+        value={email}
+        onChange={onChange}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="passwordOne"
+        value={passwordOne}
+        onChange={onChange}
+        type="password"
+        placeholder="Password"
+      />
+      <input
+        name="passwordTwo"
+        value={passwordTwo}
+        onChange={onChange}
+        type="password"
+        placeholder="Confirm Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Sign Up
+      </button>
+
+      {error && <p>{error.message}</p>}
+    </form>
+  );
 }
 
 const SignUpLink = () => (
