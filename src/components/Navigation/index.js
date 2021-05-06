e9ae50938ffb1c1e6aaf04e513371e997dd4ce91
João Ceia <joao.p.ceia@gmail.com>
2021-05-06 09:34:02 +0100

    changed navbar depending if the user in admin or not

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,32 +1,45 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
 import { AuthorizationContext } from '../Session';
 import SignOutButton from '../SignOut';
 
-const Navigation = () => (
-  <div>
-    <AuthorizationContext.Consumer>
-      {auth => auth ? <NavigationAuth /> : <NavigationNonAuth />}
-    </AuthorizationContext.Consumer>
-  </div>
-);
+const Navigation = () => {
+  const authUser = useContext(AuthorizationContext);
+  return (
+    <div>
+      {authUser ? <NavigationAuth /> : <NavigationNonAuth />}
+    </div>
+  );
+}
 
-const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
-);
+const NavigationAuth = () => {
+  const authUser = useContext(AuthorizationContext);
+
+  return (
+    <ul>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.HOME}>Home</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+      </li>
+      {
+        !!authUser.roles.includes(ROLES.ADMIN) &&
+        <li>
+          <Link to={ROUTES.ADMIN}>Admin</Link>
+        </li>
+      }
+      <li>
+        <SignOutButton />
+      </li>
+    </ul>
+  );
+}
 
 const NavigationNonAuth = () => (
   <div>
