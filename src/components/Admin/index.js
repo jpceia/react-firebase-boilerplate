5b3900291b30027fc76762e88c620dd0421b8c73
João Ceia <joao.p.ceia@gmail.com>
2021-05-06 09:33:30 +0100

    changed admin page to load the user list

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,17 +1,59 @@
-// import * as ROLES from '../../constants/roles';
+import * as ROLES from '../../constants/roles';
+import { useContext, useEffect, useState } from 'react';
+import { FirebaseContext } from '../Firebase';
 import { AuthorizationCheck } from '../Session';
 
-const condition = auth => false // auth && !!auth.roles[ROLES.ADMIN];
+const condition = user => user && !!user.roles.includes(ROLES.ADMIN);
 
-const AdminPage = () => (
-  <AuthorizationCheck condition={condition}>
-    <div>
-      <h1>Admin</h1>
-      <p>
-        Restricted area! Only users with the admin role are authorized.
-      </p>
-    </div>
-  </AuthorizationCheck>
+const AdminPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const firebase = useContext(FirebaseContext);
+
+  useEffect(() => {
+    setLoading(true);
+    return firebase.users().onSnapshot(snapshot => { // .onSnapshot
+      const usersList = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        uid: doc.id
+      }));
+      setLoading(false);
+      setUsers(usersList);
+    });
+  }, [firebase]);
+
+  return (
+    <AuthorizationCheck condition={condition}>
+      <div>
+        <h1>Admin</h1>
+        <p>
+          The Admin Page is accessible by every signed in admin user.
+        </p>
+        {loading && <div>Loading ...</div>}
+        <UsersList users={users} />
+      </div>
+    </AuthorizationCheck>
+  );
+}
+
+const UsersList = ({ users }) => (
+  <ul>
+    {
+      users.map(({ uid, email, username }) => (
+        <li key={uid}>
+          <span>
+            <strong>ID:</strong> {uid}
+          </span>
+          <span>
+            <strong>E-Mail:</strong> {email}
+          </span>
+          <span>
+            <strong>Username:</strong> {username}
+          </span>
+        </li>
+      ))
+    }
+  </ul>
 );
 
 export default AdminPage;
