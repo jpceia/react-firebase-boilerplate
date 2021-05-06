657bb851329daac1cbd69de8fed37e22dd71a6fa
João Ceia <joao.p.ceia@gmail.com>
2021-05-06 09:22:25 +0100

    added firestore support

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,12 +1,16 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 import config from './config';
 
 class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
+
+  // *** Auth API ***
 
   doCreateUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
@@ -19,6 +23,42 @@ class Firebase {
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  doSendEmailVerification = () =>
+    this.auth.currentUser.sendEmailVerification({
+      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+    });
+
+  // *** Merge Auth and DB User API *** //
+
+  onAuthUserListener = (next, fallback) =>
+    this.auth.onAuthStateChanged(authUser => {
+      if (authUser)
+        this.user(authUser.uid)
+          .get()
+          .then(doc => {
+            const dbUser = doc.data();
+
+            // default empty roles
+            if (!dbUser.roles)
+              dbUser.roles = [];
+
+            // merge auth and db user
+            authUser = {
+              uid: authUser.uid,
+              email: authUser.email,
+              ...dbUser,
+            };
+
+            next(authUser);
+          });
+      else
+        fallback();
+    });
+
+  // *** User API ***
+  users = () => this.db.collection('users');
+  user = (uid) => this.users().doc(uid);
 }
 
 export default Firebase;
